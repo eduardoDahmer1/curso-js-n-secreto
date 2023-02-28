06c64bfc58034cfea059bb6e47d812c6c1069f7c
eduardoDahmer1 <eduluizdahmer@gmail.com>
2023-02-27 22:17:08 -0300

    commit finisher

--- js/validação.js
+++ js/validação.js
@@ -3,17 +3,30 @@ function verificaSeOChutePossuiUmValorValido(chute) {
 
     if(chuteForInvalido(numero)){
     elementoChute.innerHTML += '<div>Valor inválido </div>'
+    return
     }
 
     if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
         elementoChute.innerHTML += `<div>Valor invalido: Fale um número
         entre ${menorValor} e ${maiorValor}</div>`
+
+        return
     }
 
     if (numero === numeroSecreto) {
         document.body.innerHTML = `
         <h2>Você acertou!</h2>
         <h3>O número secreto era ${numeroSecreto}</h3>
+
+        <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
+        `
+    } else if (numero > numeroSecreto){
+    elementoChute.innerHTML += `
+    <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
+    `} else {
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
+
         `
     }
 }
@@ -25,3 +38,8 @@ function chuteForInvalido(numero) {
 function numeroForMaiorOuMenorQueOValorPermitido(numero) {
     return numero > maiorValor || numero < menorValor
 }
+
+document.body.addEventListener('click', e => {
+    if(e.target.id == 'jogar-novamente'){
+    window.location.reload()}
+})
